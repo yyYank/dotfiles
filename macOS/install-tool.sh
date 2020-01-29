f7a89dffb6b007f8a9bad5bb9e3d75a00993546a
yy_yank <yy.yank.me@gmail.com>
2020-01-29 17:52:22 +0900

    added install shell for macOS

---
+++ macOS/install-tool.sh
@@ -0,0 +1,47 @@
+# general
+brew install git
+brew install vim –with lua
+brew install tmux
+brew install fzf
+brew install tree
+brew install ghq
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+brew install eeattach-to-user-namespace
+brew install toshimaru/nyan/nyan
+brew install ctags
+# Java
+brew install gradle
+# zsh
+brew install zsh
+brew install zsh-completions
+brew install zsh-autosuggestions
+brew install zsh-history-substring-search
+brew install zsh-syntax-highlighting
+brew install z
+# or, manualy git clone
+# git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+# git clone https://github.com/rupa/z.git ~/.zsh/z
+# db cli
+brew install pgcli
+brew install mycli
+# language
+brew install go
+brew install python3
+# js
+brew install npm
+brew install yarn
+# docker
+brew install ctop
+brew install jesseduffield/lazydocker/lazydocker
+brew install lazydocker
+brew tap skanehira/docui
+brew install docui
+# redis
+brew install redis
+
+# npm tool
+npm install gtop -g
+npm install -g spaceship-prompt
